86e47c683119369fdf9675a452f4bb14032c0801
Oscar Herrera <a.oscarherrera@gmail.com>
2024-06-24 10:26:16 -0500

    mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['ui-sans-serif', 'system-ui' ],
+        'serif': ['baskervville', 'Georgia' ],
+        'mono': ['ui-monospace', 'SFMono-Regular'],
+        'display': ['Oswald'],
+        'body': ['"Open Sans"'],
+      },
+      backgroundImage: {
+        'background-hero': "url('./assets/background-img.jpg')"
+      }
+    },
   },
   plugins: [],
 }
